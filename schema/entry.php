e87e6f17d4422af8fcf74c0398e979431ba3306b
Cristian Dobre <cristian.dobre@hbfsrobotics.com>
2026-02-23 10:05:11 +0200

    Create entry.php

---
+++ schema/entry.php
@@ -0,0 +1,17 @@
+<?php 
+
+class PB_Entry {
+    public $id;
+    public $skey;
+    public $content;
+    public $lang;
+    public $date_published;
+    public $date_modified;
+    public $credit;
+    public $draft;
+    public $cleanup;
+    public $creator;
+    public $category;
+    public $translation;
+}
+
